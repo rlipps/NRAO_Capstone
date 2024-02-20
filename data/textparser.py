0679775089af8cbf0fec1f3f4e3c4a6db0432e49
Noah McIntire <ne19mc@gmail.com>
2024-02-20 16:17:15 -0500

    built out textparser.py and new examples in Data_Preprocessing.ipynb

--- data/textparser.py
+++ data/textparser.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.lancaster import LancasterStemmer
 
 class TextParser():
     """
@@ -31,9 +34,9 @@ class TextParser():
         'm': '_id'
     }
         
-    def __init__(self, text, use_nltk=True):
+    def __init__(self, text_df, use_nltk=True):
         """Initialize the object and extract config info. If using NLTK, download resources."""
-        self.text = text
+        self.text_df = text_df
         self.OHCO = [item[0]+self._ohco_type[item[2]] for item in self.ohco_pats]
         self.ohco_names = [item[0] for item in self.ohco_pats]
         self.use_nltk = use_nltk
@@ -56,12 +59,40 @@ class TextParser():
                     nltk.data.find(package)
                 except IndexError:
                     nltk.download(package)    
+    
+    def parse_tokens(self):
+        books = []
+        # Define Parameters
+        self.verbose = True
+        self.strip_hyphens = True
+        self.strip_whitespace = True
+        for x in range(len(self.text_df.index)):
+        # parse_tokens
+            self._parse_text(self.text_df.iloc[x]['project_title_abstract'])
+
+        # Name things
+            self.TOKENS['project_code'] = self.text_df.iloc[x]['project_code']
+            self.TOKENS = self.TOKENS.reset_index().set_index(['project_code'] + self.OHCO)
+
+        # Add to list
+            books.append(self.TOKENS)
+        
+    # Combine into a single dataframe
+        self.CORPUS = pd.concat(books).sort_index()
+        self.CORPUS['pos_group'] = self.CORPUS.pos.str[:2]
+
+    # Clean up
+        del(books)
+        
+        print("Done")
+        
+        return self.CORPUS
 
         
-    def parse_tokens(self):
-        """Convert lines to tokens based on OHCO."""
+    def _parse_text(self, text):
+        """Convert lines to tokens based on OHCO. Should only be accessed by parse_tokens."""
             # Start with the LINES df
-        self.TOKENS = self.text
+        self.TOKENS = text
 
             # Walk through each level of the OHCO to build out TOKENS
         for i, level in enumerate(self.OHCO):
@@ -143,17 +174,112 @@ class TextParser():
             self.TOKENS['term_str'] = self.TOKENS[~self.TOKENS.pos.isin(punc_pos)].token_str\
                 .str.replace(r'[\W_]+', '', regex=True).str.lower()  
 
-    def extract_vocab(self):
-        """This should also be done at the corpus level."""
-        self.VOCAB = self.TOKENS.term_str.value_counts().to_frame('n')
+    def extract_vocab(self, pos_ambig=False, stems='none'):
+        """
+        Extracts VOCAB, and saves/returns a vocab table. Includes parameters for customization
+        that include:
+        
+        :param bool pos_ambig: Option to include part of speech ambiquity columns. The default is False.
+        :param str stems: Option to include columns of various stemmed versions each token. Options include:
+        'none' - No stemmed columns added
+        'all' - All the following stemmed values are included in the vocab table.
+        'porter' - include a column that performs the porter stemming algorithm on the tokens in the table.
+        'snowball' - include a column that performs the snowball stemming algorithm on the tokens in the table.
+        'lancaster' - include a column that performs the lancaster stemming algorithm on the tokens in the table.
+        """
+        self.VOCAB = self.CORPUS.term_str.value_counts().to_frame('n').sort_index()
         self.VOCAB.index.name = 'term_str'
         self.VOCAB['n_chars'] = self.VOCAB.index.str.len()
-        self.VOCAB['p'] = self.VOCAB['n'] / self.VOCAB['n'].sum()
-        self.VOCAB['s'] = 1 / self.VOCAB['p']
-        self.VOCAB['i'] = np.log2(self.VOCAB['s']) # Same as negative log probability (i.e. log likelihood)
-        self.VOCAB['h'] = self.VOCAB['p'] * self.VOCAB['i']
-        self.H = self.VOCAB['h'].sum()
-        return self
+        self.VOCAB['p'] = self.VOCAB.n / self.VOCAB.n.sum()
+        self.VOCAB['i'] = -np.log2(self.VOCAB.p)
+        if pos_ambig:
+            # Implement pos_ambig for vocab table
+            self.VOCAB['n_pos_group'] = self.CORPUS[['term_str','pos_group']].value_counts().unstack().count(1)
+            self.VOCAB['cat_pos_group'] = self.CORPUS[['term_str','pos_group']].value_counts().to_frame('n').reset_index()\
+    .groupby('term_str').pos_group.apply(lambda x: set(x))
+            self.VOCAB['n_pos'] = self.CORPUS[['term_str','pos']].value_counts().unstack().count(1)
+            self.VOCAB['cat_pos'] = self.CORPUS[['term_str','pos']].value_counts().to_frame('n').reset_index()\
+    .groupby('term_str').pos.apply(lambda x: set(x))
+        if stems == 'all':
+            stemmer1 = PorterStemmer()
+            self.VOCAB['stem_porter'] =   self.VOCAB.apply(lambda x: stemmer1.stem(x.name), 1)
+            stemmer2 = SnowballStemmer("english")
+            self.VOCAB['stem_snowball'] = self.VOCAB.apply(lambda x: stemmer2.stem(x.name), 1)
+            stemmer3 = LancasterStemmer()
+            self.VOCAB['stem_lancaster'] = self.VOCAB.apply(lambda x: stemmer3.stem(x.name), 1)
+            return self.VOCAB
+        elif stems == 'porter':
+            stemmer1 = PorterStemmer()
+            self.VOCAB['stem_porter'] =   self.VOCAB.apply(lambda x: stemmer1.stem(x.name), 1)
+            return self.VOCAB
+        elif stems == 'snowball':
+            stemmer2 = SnowballStemmer("english")
+            self.VOCAB['stem_snowball'] = self.VOCAB.apply(lambda x: stemmer2.stem(x.name), 1)
+            return self.VOCAB
+        elif stems == 'lancaster':
+            stemmer3 = LancasterStemmer()
+            self.VOCAB['stem_lancaster'] = self.VOCAB.apply(lambda x: stemmer3.stem(x.name), 1)
+            return self.VOCAB
+        else:
+            return self.VOCAB
+        
+    def corp_to_BOW(self, bag_OHCO=1):
+        '''Takes a tokenized data frame and outputs a bag of word representation
+        based off a desired OHCO.
+    
+        :param pd.df token_df: A tokenized df to be transformed into a BOW representation.
+        :param int bag_OHCO: An interger representing the level at which the bagging should 
+        occur. Options include:
+        1 = project_code
+        2 = sent_num
+        '''
+        OHCO = ['project_code', 'sent_num', '']
+        bags = OHCO[:bag_OHCO]
+        self.Bag = self.CORPUS.groupby(bags +['term_str']).term_str.count().to_frame('n') 
+        return self.Bag
+    
+    def BOW_TFIDF(self, tf_method):
+        '''Takes a BOW data frame and outputs df with desired TFIDF values.
+    
+        :param pd.df bow_df: A BOW df to add desired TFIDF values.
+        :param str tf_method: Desired tf_type calculation. Options include:
+        sum, max, log, double_norm, raw, binary
+        '''
+        self.BOW = self.Bag.copy(deep=True)
+        DTCM = self.BOW.n.unstack(fill_value=0)
+        # Select tf_method
+        if tf_method == 'sum':
+            TF = DTCM.T / DTCM.T.sum()
+
+        elif tf_method == 'max':
+            TF = DTCM.T / DTCM.T.max()
+    
+        elif tf_method == 'log':
+            TF = np.log2(1 + DTCM.T)
+    
+        elif tf_method == 'raw':
+            TF = DTCM.T
+    
+        elif tf_method == 'double_norm':
+            TF = DTCM.T / DTCM.T.max()
+    
+        elif tf_method == 'binary':
+            TF = DTCM.T.astype('bool').astype('int')
+   
+        TF = TF.T 
+        N = DTCM.shape[0]
+        DF = DTCM.astype('bool').sum() 
+        # Compute IDF with standard method
+        IDF = np.log2(N / DF)
+        # Compute IDF
+        TFIDF = TF * IDF
+        # Add to return table and return
+        self.BOW['tf'] = TF.stack()
+        self.BOW['tfidf'] = TFIDF.stack()
+        return self.BOW
+    
+    
+    
 
     def gather_tokens(self, level=0, grouping_col='term_str', cat_sep=' '):
         """Gather tokens into strings for arbitrary OHCO level."""
